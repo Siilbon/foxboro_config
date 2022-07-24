bbd489ef2373c5a2246c8caa201b8106c5d0c7f2
Karl Siil <karlsiil@karls-mbp-2.lan>
2022-07-24 10:31:32 -0500

    Created FoxboroConfig class
    Wrapped the function in a class to make it easier to use for multiple files

--- foxboro_parse.py
+++ foxboro_parse.py
@@ -3,45 +3,55 @@ import pandas as pd
 import re
 
 #%%
-foxboro_raw = 'raw_data/testdata.out'
 
 
-with open(foxboro_raw, encoding='latin') as f:
-    # patterns for Foxboro config files
-    new_block_pattern = r'^NAME\s+=\s+(?P<compound>\w+):(?P<name>\w+)'
-    block_param_pattern = r'\s+(?P<param>\w+)\s+=\s+(?P<value>.*)$'
-    end_block_pattern = r'^END$'
+class FoxboroConfig:
 
-    # Build a dictionary containing the data for a block
-    blocks = {}
-    block_dict = {}
-    block_counter = 0
-    for line in f:
-        # Find a new block
-        if name := re.search(new_block_pattern, line):
-            block_dict['COMPOUND'] = name.group('compound')
-            block_dict['NAME'] = name.group('name')
+    def __init__(self, path):
+        self.path = path
+        self.dfs = self.parse()
 
-        # Find the block parameters
-        elif block_param := re.search(block_param_pattern, line):
-            param = block_param.group('param')
-            value = block_param.group('value')
-            block_dict[param] = value
+    def parse(self):
+        '''Take the path to a Foxboro config file and convert it
+        into a dictionary of pandas DataFrames'''
 
-        # At the end of a block add it to a dataframe
-        elif end := re.search(end_block_pattern, line):
-            block_type = block_dict['TYPE']
-            block_df = pd.DataFrame(block_dict, index=[0])
+        with open(self.path, encoding='latin') as f:
+            # patterns for Foxboro config files
+            new_block_pattern = r'^NAME\s+=\s+(?P<compound>\w+):(?P<name>\w+)'
+            block_param_pattern = r'\s+(?P<param>\w+)\s+=\s+(?P<value>.*)$'
+            end_block_pattern = r'^END$'
 
-            if block_type not in blocks:
-                blocks[block_type] = block_df
-            else:
-                blocks[block_type] = pd.concat([blocks[block_type], block_df], ignore_index=True)
-            
-            # count the new block and reset 
-            block_counter += 1
+            # Build a dictionary containing the data for a block
+            blocks = {}
             block_dict = {}
-    
-    print(f'Gathered data for {block_counter} blocks')
+            block_counter = 0
+            for line in f:
+                # Find a new block
+                if name := re.search(new_block_pattern, line):
+                    block_dict['COMPOUND'] = name.group('compound')
+                    block_dict['NAME'] = name.group('name')
 
-#%%
+                # Find the block parameters
+                elif block_param := re.search(block_param_pattern, line):
+                    param = block_param.group('param')
+                    value = block_param.group('value')
+                    block_dict[param] = value
+
+                # At the end of a block add it to a dataframe
+                elif end := re.search(end_block_pattern, line):
+                    block_type = block_dict['TYPE']
+                    block_df = pd.DataFrame(block_dict, index=[0])
+
+                    if block_type not in blocks:
+                        blocks[block_type] = block_df
+                    else:
+                        blocks[block_type] = pd.concat(
+                            [blocks[block_type], block_df], ignore_index=True)
+
+                    # count the new block and reset
+                    block_counter += 1
+                    block_dict = {}
+
+            print(f'Gathered data for {block_counter} blocks')
+
+        return blocks
